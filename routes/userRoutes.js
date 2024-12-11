36eca001b2f5805a2b126db164be459042456fc5
Nafta <161855796+wilmanProyect@users.noreply.github.com>
2024-12-11 00:55:07 -0400

    Rutas del usuario, Registro y login

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {registerUser, loginUser} = require('../controller/userController');
+const router = express.Router();
+
+router.post('/regiter',registerUser);
+router.post('/login',loginUser);
+
+module.exports = router;
