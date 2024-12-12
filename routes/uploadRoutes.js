3263042841685243c61fd9a892385858ef0fe649
Nafta <161855796+wilmanProyect@users.noreply.github.com>
2024-12-12 16:02:36 -0400

    eliminamos archivos de prueba(innecesarios)

--- routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { upload, uploadFile } = require('../controller/uploadController');
-const { protect, admin, creator } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-
-router.post('/upload', upload.single('file'), uploadFile, protect, admin, creator);
-
-module.exports = router;
